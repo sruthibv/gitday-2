14f0844d4d172334ca913e95b10ca98c551f46b1
bsruthibv <bsruthiuk@gmail.com>
2025-04-08 09:51:05 +0100

    new app.py file created

---
+++ app.py
@@ -0,0 +1,22 @@
+# This is a practice test subject 
+
+# Here " Im adding details so that, I can able to see this in my app.py in remote repository"
+
+# When can I able to see the result ? 
+
+" If you want to add any file, first you need to add in staging area and then commit to your local repo (which is your current laptop you are working now) by git add command and then commit (commit here is actually making you to add into your local repo - that means your laptop gain) and with git log you can see all the commit updates" 
+
+# Git add - to add the code you developed in working directory to staging area
+
+# Git commit - to add the file you developed in stagin area to local repo. 
+
+# git log - you can the commit updates with this command
+
+# Git push is actually to push to your remote repository.  
+
+
+# This is for my understanding and to check the changes I may change in future and check the reflections in remote repository
+
+
+
+present - Hello this new update. 
